95ad3db89b66dd35c400e6ead30096e8cb23083d
Dan Damit <167915895+dan-damit@users.noreply.github.com>
2025-10-27 11:22:36 -0500

    adjusting naming of script

--- PowerShell/w32tmClientConfig.ps1
+++ PowerShell/w32tmClientConfig.ps1
@@ -1,5 +1,5 @@
-# Set external NTP source
-w32tm /config /manualpeerlist:"time.windows.com,0x1" /syncfromflags:manual /reliable:YES /update
+# Set external NTP source and only used if NTP server isn't found locally
+w32tm /config /syncfromflags:all /manualpeerlist:"time.windows.com,0x1" /reliable:YES /update
 
 # Restart time service
 Restart-Service w32time
@@ -7,8 +7,8 @@ Restart-Service w32time
 # Define task action
 # C:\Windows\System32 must be registered in at least the current session PATH
 # Ideally register it system wide for future task scheduled event
-# Or use full pth in the -Execute param
-$action = New-ScheduledTaskAction -Execute "w32tm.exe" -Argument "/resync"
+# Or use full path in the -Execute param
+$action = New-ScheduledTaskAction -Execute "C:\Windows\System32\w32tm.exe" -Argument "/resync"
 
 # Define trigger: daily at 12:00 PM
 $trigger = New-ScheduledTaskTrigger -Daily -At 12:00PM
