0a337e6e677d6a9a30aae25fbb34e9cf72ab0c52
Dan Damit <167915895+dan-damit@users.noreply.github.com>
2025-10-27 11:05:33 -0500

    Update w32tmConfig.ps1

--- PowerShell/w32tmConfig.ps1
+++ PowerShell/w32tmConfig.ps1
@@ -5,6 +5,9 @@ w32tm /config /manualpeerlist:"time.windows.com,0x1" /syncfromflags:manual /reli
 Restart-Service w32time
 
 # Define task action
+# C:\Windows\System32 must be registered in at least the current session PATH
+# Ideally register it system wide for future task scheduled event
+# Or use full pth in the -Execute param
 $action = New-ScheduledTaskAction -Execute "w32tm.exe" -Argument "/resync"
 
 # Define trigger: daily at 12:00 PM
