e861e46d326e3effb7f9a18df58b2464965da436
Dan Damit <167915895+dan-damit@users.noreply.github.com>
2025-11-10 10:08:03 -0600

    Create ScheduleReboot.ps1

---
+++ PowerShell/ScheduleReboot.ps1
@@ -0,0 +1,3 @@
+$action = New-ScheduledTaskAction -Execute "shutdown.exe" -Argument "/r /t 0"
+$trigger = New-ScheduledTaskTrigger -Once -At "20:00"
+Register-ScheduledTask -TaskName "ScheduledReboot8PM" -Action $action -Trigger $trigger -Description "Daily reboot at 8PM" -User "SYSTEM" -RunLevel Highest -Force
